c6d7c5e89aa0c2ce3113c32060c8dd5758e616eb
SouthernKnight <github@blandfamily.com>
2017-05-09 04:55:29 -0500

    Adding new lesson

--- lesson6/main.pl
+++ lesson6/main.pl
@@ -2,11 +2,40 @@
 use strict;
 use warnings FATAL => 'all';
 
-$|=1;
+$| = 1;
+
+sub readWriteFile() {
+    my $inputfile = '../lesson5/mymanjeeves.txt';
+    my $outputfile = 'mymanjeeves_extract.txt';
+
+    open(INPUT, $inputfile) or die("Input file not found: $inputfile\n");
+    open(OUTPUT, '>'.$outputfile) or die("Output file not created: $outputfile\n");
+
+    while (my $line = <INPUT>)
+    {
+        if ($line =~ /\begg\b/)
+        {
+            $line =~ s/you/YOU/ig;
+            print OUTPUT $line;
+        }
+    }
+
+    close(INPUT);
+    close(OUTPUT);
+}
+
 
 sub main()
 {
-    
+    my $output = 'output.txt';
+
+    open(OUTPUT, '>'.$output) or die("Can't create output file: $output\n");
+
+    print OUTPUT "Hello\n";
+
+    close(OUTPUT);
+
+    readWriteFile();
 }
 
 main();
